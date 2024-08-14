62c5b8e3b16128d2bd832f8fe3d2744b71af5be0
ghosh-trisha <trisha.ghosh1620@gmail.com>
2024-08-15 01:03:34 +0530

    loginModule

--- src/routers/loginUser.router.js
+++ src/routers/loginUser.router.js
@@ -1,10 +1,11 @@
 
 import express from 'express'
 import authMiddleware from '../middlewares/auth.middleware.js'
-import {registerUser,loginUser,logoutUser,updatePassword} from '../controllers/LoginUser.controller.js'
+import {sendOtp, registerUser,loginUser,logoutUser,updatePassword} from '../controllers/LoginUser.controller.js'
 const router = express.Router();
 
 
+router.post('/send-otp', sendOtp)
 router.post('/register', registerUser)
 router.post('/login', loginUser)
 router.post('/logout', authMiddleware, logoutUser)
