d5b0d158d16822aeaca6308baa7055d61f0c8155
Biman Kumar Das <daskumarbiman2020@gmail.com>
2024-08-16 10:55:28 +0530

    code edit

---
+++ src/middlewares/auth.check.middleware.js
@@ -0,0 +1,42 @@
+
+import ApiError from '../utils/ApiError.js'
+import AsyncHandler from '../utils/AsyncHandler.js'
+import LoginUser from '../models/LoginUser.model.js'
+import jwt from "jsonwebtoken"
+
+
+const authCheckMiddleware = AsyncHandler(async (req, res, next)=>{
+
+
+    // Retrieve token from cookies or Authorization header
+    const token = req.cookies?.accessToken||req.header("Authorization")?.replace("Bearer ","")
+    if(!token)
+    {
+        req.auth=false;
+        next();
+        // throw new ApiError(401,"Unauthorized user")
+    }
+    
+
+    //verify token
+    const tokenData=jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+
+
+    //checking and getting user
+    const user=await LoginUser.findById(tokenData?._id)
+    if(!user)
+    {
+        req.auth=false;
+        next();
+    }
+
+
+    //adding user to request object
+    req.user=user
+    // console.log(req.user);
+    req.auth=true;
+    next()
+})
+
+
+export default authCheckMiddleware
