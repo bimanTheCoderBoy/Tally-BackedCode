cf848e40b09da2e4744d8ec14eb1ad92b6191bad
ghosh-trisha <trisha.ghosh1620@gmail.com>
2024-08-17 17:30:46 +0530

    commited

---
+++ src/controllers/recommendation.controller.js
@@ -0,0 +1,13 @@
+
+import mongoose from "mongoose";
+import AsyncHandler from '../utils/AsyncHandler.js'
+import ApiError from '../utils/ApiError.js'
+import ApiResponse from '../utils/ApiResponse.js'
+
+
+const recommendation = AsyncHandler( (req, res, next) => {
+    
+} );
+
+
+export default recommendation
---
+++ src/routers/recommendation.router.js
@@ -0,0 +1,11 @@
+
+import express from 'express';
+import authMiddleware from '../middlewares/auth.middleware.js';
+import authCheckMiddleware from '../middlewares/auth.check.middleware.js';
+const router = express.Router();
+
+
+router.post();
+
+
+export default router;
